d92df5f7036b27274e7f8235277dc1dfc2074a66
seodevv <seodev17@gmail.com>
2025-10-14 02:09:46 +0900

    testing

--- src/app.ts
+++ src/app.ts
@@ -34,76 +34,76 @@ if (!fs.pathExistsSync(uploadPath)) {
   fs.mkdirSync(uploadPath);
 }
 
-if (cluster.isPrimary && process.env.NODE_ENV !== 'test') {
-  console.log(`Primary ${process.pid} is running`);
-  console.log('origin', origin);
+// if (cluster.isPrimary && process.env.NODE_ENV !== 'test') {
+//   console.log(`Primary ${process.pid} is running`);
+//   console.log('origin', origin);
 
-  initializeDatabase()
-    .then(() => {
-      const MAX_WORKER =
-        process.env.MAX_WORKER && ~~process.env.MAX_WORKER !== 0
-          ? ~~process.env.MAX_WORKER
-          : 1;
-      const num_worker =
-        os.cpus().length > MAX_WORKER ? MAX_WORKER : os.cpus().length;
+//   initializeDatabase()
+//     .then(() => {
+//       const MAX_WORKER =
+//         process.env.MAX_WORKER && ~~process.env.MAX_WORKER !== 0
+//           ? ~~process.env.MAX_WORKER
+//           : 1;
+//       const num_worker =
+//         os.cpus().length > MAX_WORKER ? MAX_WORKER : os.cpus().length;
 
-      for (let i = 0; i < num_worker; i++) {
-        cluster.fork();
-      }
-    })
-    .catch(console.error);
+//       for (let i = 0; i < num_worker; i++) {
+//         cluster.fork();
+//       }
+//     })
+//     .catch(console.error);
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died. Restarting...`);
-    cluster.fork();
-  });
-} else {
-  const app = express();
-  app.use(
-    cors({
-      origin,
-      optionsSuccessStatus: 200,
-      credentials: true,
-    })
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`Worker ${worker.process.pid} died. Restarting...`);
+//     cluster.fork();
+//   });
+// } else {
+const app = express();
+app.use(
+  cors({
+    origin,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  morgan(
+    `[Worker ${process.pid}] :remote-addr :method :url :status :res[content-length] - :response-time ms`
+  )
+);
+app.use('/api', apiRouter);
+
+server =
+  process.env.NODE_ENV === 'production'
+    ? http.createServer({}, app)
+    : https.createServer(
+        {
+          key: fs.readFileSync('./localhost-key.pem'),
+          cert: fs.readFileSync('./localhost.pem'),
+        },
+        app
+      );
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>(server, {
+  cors: {
+    origin,
+    methods: ['GET', 'POST'],
+  },
+  maxHttpBufferSize: 1e9,
+});
+setupSocket(io);
+
+server.listen(port, host, async () => {
+  console.log(
+    `Worker ${process.pid} is running on : ${
+      process.env.NODE_ENV === 'production' ? 'http' : 'https'
+    }://${host}:${port}`
   );
-  app.use(express.json());
-  app.use(cookieParser());
-  app.use(
-    morgan(
-      `[Worker ${process.pid}] :remote-addr :method :url :status :res[content-length] - :response-time ms`
-    )
-  );
-  app.use('/api', apiRouter);
-
-  server =
-    process.env.NODE_ENV === 'production'
-      ? http.createServer({}, app)
-      : https.createServer(
-          {
-            key: fs.readFileSync('./localhost-key.pem'),
-            cert: fs.readFileSync('./localhost.pem'),
-          },
-          app
-        );
-  const io = new Server<
-    ClientToServerEvents,
-    ServerToClientEvents,
-    InterServerEvents,
-    SocketData
-  >(server, {
-    cors: {
-      origin,
-      methods: ['GET', 'POST'],
-    },
-    maxHttpBufferSize: 1e9,
-  });
-  setupSocket(io);
-
-  server.listen(port, host, async () => {
-    console.log(
-      `Worker ${process.pid} is running on : ${
-        process.env.NODE_ENV === 'production' ? 'http' : 'https'
-      }://${host}:${port}`
-    );
-  });
-}
+});
+// }
