8ecefa9028669b443fa0179a5a5f8d621f77ed7c
seodevv <seodev17@gmail.com>
2025-10-14 14:36:48 +0900

    fix middleware

--- src/app.ts
+++ src/app.ts
@@ -22,9 +22,9 @@ import {
 
 const host = process.env.SERVER_HOST || '0.0.0.0';
 const port = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 9090;
-const origin = process.env.SERVER_ORIGIN
-  ? process.env.SERVER_ORIGIN
-  : 'https://localhost';
+// const origin = process.env.SERVER_ORIGIN
+//   ? process.env.SERVER_ORIGIN
+//   : 'https://localhost';
 
 export let server: ReturnType<(typeof https | typeof http)['createServer']>;
 export const uploadPath = path.join(__dirname, '../uploads');
@@ -59,14 +59,13 @@ if (!fs.pathExistsSync(uploadPath)) {
 // } else {
 const app = express();
 app.set('trust proxy', true);
-app
-  .use
-  // cors({
-  //   origin,
-  //   optionsSuccessStatus: 200,
-  //   credentials: true,
-  // })
-  ();
+// app.use(
+//   cors({
+//     origin,
+//     optionsSuccessStatus: 200,
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 app.use(cookieParser());
 app.use(
